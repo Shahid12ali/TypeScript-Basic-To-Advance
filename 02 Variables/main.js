e7588e637bc9ffdf6b248d685380adff257ade39
Shahid Ali <153486921+Shahid12ali@users.noreply.github.com>
2024-05-30 19:36:49 +0500

    Declaring. Using. And Changing A Variables.

---
+++ 02 Variables/main.js
@@ -0,0 +1,13 @@
+// What is Variable: ?
+// • Variables Can Store Some information. We can use that information. We Can Change That Information
+// 1• Declaring A Variable
+// in string we can use single qoute, double quote, and backticks
+// var firstName : string = "Shahid Ali";
+var firstName = "Shahid Ali";
+// const firstName : string = "Shahid Ali";
+//We Can use three types of variables. var, let, and const
+// 2• Using A Variable
+console.log(firstName);
+// 3• Changing A Variable
+firstName = "Hello World";
+console.log(firstName);
