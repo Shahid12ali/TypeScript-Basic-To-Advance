eaf1d1f98469bf354cc46884f772dd6c7a79231b
Shahid Ali <153486921+Shahid12ali@users.noreply.github.com>
2024-08-05 20:49:28 +0500

    Operators

---
+++ 06-B- Operators/main.ts
@@ -0,0 +1,70 @@
+// 3. != Not equal to (loose comparison)
+let num1 : number = 30;
+let num2 : string = "30";
+// console.log(num1 != num2); // Output false, ignore for time bieng
+// check values only
+
+
+
+// 4. !== Strictly Not equal to (check type and value, recommended for strict comparison);
+let numb1 : number = 30;
+let numb2 : string = "30";
+// console.log(numb1 !== numb2);
+// Check type and values both
+
+
+
+// 5. ' < ' less than
+let num3 : number = 15;
+let num4 : number = 30;
+console.log(num3 < num4);  //  Output true
+
+
+
+// 6. ' > ' greater than
+let num5 : number = 15;
+let num6 : number = 30;
+console.log(num5 > num6);  //  Output false
+
+
+
+// 7. ' <= ' less than or equal to
+let Num1 : number = 30;
+let Num2 : number = 30;
+console.log(Num1 <= Num2);  //  Output true
+
+
+
+// 8. ' >= ' greater than or equal to
+let Num3 : number = 30;
+let Num4 : number = 30;
+console.log(Num3 >= Num4);  //  Output true
+
+
+
+
+// Logical Operators:
+
+// 1. ' && ' And (both sides must be true for the expression to be true);
+
+let num_1 : number = 55;
+let num_2 : number = 55;
+let num_3 : number = 8;
+let num_4 : number = 5;
+console.log(num_1 === num_2 && num_3 === num_4);  // Output false;
+
+
+
+// 2. ' || ' or (at least one side must be true for the expression to be true);
+let num$1 : number = 55;
+let num$2 : number = 55;
+let num$3 : number = 8;
+let num$4 : number = 5;
+console.log(num$1 === num$2 || num$3 === num$4);   // Output true;
+
+
+
+// 3. ' ! ' Not (inverts the truth value);
+let numbe1 : number = 40;
+let numbe2 : number = 10;
+console.log(numbe1 !== numbe2);     // Output true;
